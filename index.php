cccebbd43b5d0caa65d18e03562984bcfd5d71a4
Joel Santana <p3t3ca5206@gmail.com>
2020-03-19 14:27:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+
+        <title>Button</title>
+    </head>
+    <body>
+        <div class="content">
+            <button>
+                <div class="btn-content">
+                    <div class="btn-left">
+                        <div class="card">
+                            <div class="card-top"></div>
+                            <div class="card-bottom">
+                                <div class="ball1"></div>
+                                <div class="ball2"></div>
+                                <div class="ball3"></div>
+                            </div>
+                        </div>
+
+                        <div class="machine">
+                            <div class="machine-top">
+                                <div class="put-card"></div>
+                            </div>
+                            <div class="machine-content">
+                                <div class="machine-content-top"></div>
+                                <div class="screen">
+                                    <div class="money">
+                                        $
+                                    </div>
+                                </div>
+                                <div class="keyboard">
+                                    <div class="button-machine-top">
+                                        <div class="button-machine"></div>
+                                        <div class="button-machine button-machine-center"></div>
+                                        <div class="button-machine"></div>
+                                    </div>
+                                    <div class="button-machine-bottom">
+                                        <div class="button-machine-int"></div>
+                                        <div class="button-machine-int button-machine-center"></div>
+                                        <div class="button-machine-int"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="btn-right"></div>
+                </div>
+            </button>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
